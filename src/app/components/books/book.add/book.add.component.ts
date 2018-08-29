a09d8475eb26b38d7f225d097684f1802a4b56a1
DK_Antikorn <dk_antikorn@hotmail.com>
2018-08-29 12:02:25 +0700

    no message

--- src/app/components/books/book.add/book.add.component.ts
+++ src/app/components/books/book.add/book.add.component.ts
@@ -1,6 +1,6 @@
 import { BookService } from './../../../services/book.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-book.add',
   templateUrl: './book.add.component.html',
@@ -10,15 +10,22 @@ export class BookAddComponent implements OnInit {
 
   private name: string;
 
-  constructor(private _bookservice: BookService) { }
+  constructor(private _bookservice: BookService, private router: Router) { }
 
   ngOnInit() {
+
   }
 
+  /**
+   * 
+   * Function add the new item of the books
+   * @author  sarawutt.b
+   */
   addBook() {
     console.log(this.name);
-    this._bookservice.add(this.name).subscribe(res=>{
+    this._bookservice.add(this.name).subscribe(res => {
       console.log(res);
+      this.router.navigate(['/books']);
     });
   }
 
